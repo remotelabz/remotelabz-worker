0452f116d8b1da6134a33ac2a2c2b5904762a5e4
Florent NOLOT <florent.nolot@univ-reims.fr>
2025-12-28 09:43:54 +0000

    modify update process

--- bin/remotelabz-worker-update.sh
+++ bin/remotelabz-worker-update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd /opt/remotelabz-worker
 git fetch
-mv /opt/remotelabz/config/packages/messenger.yaml ~/
-git restore /opt/remotelabz/config/packages/messenger.yaml
-mv /opt/remotelabz/config/packages/dev/web_profiler.yaml ~/
-git restore /opt/remotelabz/config/packages/dev/web_profiler.yaml
+mv /opt/remotelabz-worker/config/packages/messenger.yaml ~/
+git restore /opt/remotelabz-worker/config/packages/messenger.yaml
+mv /opt/remotelabz-worker/config/packages/dev/web_profiler.yaml ~/
+git restore /opt/remotelabz-worker/config/packages/dev/web_profiler.yaml
 git pull
-mv ~/messenger.yaml /opt/remotelabz/config/packages/messenger.yaml
-mv ~/web_profiler.yaml /opt/remotelabz/config/packages/dev/web_profiler.yaml
+mv ~/messenger.yaml /opt/remotelabz-worker/config/packages/messenger.yaml
+mv ~/web_profiler.yaml /opt/remotelabz-worker/config/packages/dev/web_profiler.yaml
 composer update
 php bin/console cache:clear
 chown remotelabz-worker:www-data * -R
